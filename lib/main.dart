27d6858a6d0a2b97088b5d829cdda14a5ce3503c
pearlbob <bob@bsteele.com>
2020-08-20 22:53:37 -0700

    rude rendering from font

--- lib/main.dart
+++ lib/main.dart
@@ -117,7 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   willChange: false,
                   child: SizedBox(
                     width: double.infinity,
-                    height: 600.0,
+                    height: 800.0,
                   ),
                 ),
               ),
@@ -134,20 +134,86 @@ const double staffSpace = 40;
 class _PlotPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
+    _canvas = canvas;
     //  clear the plot
     canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), _white);
 
+    double staffLineThickness = EngravingDefaults.staffLineThickness / 2; //  style basis only
+
     //  practice staff
-    final double yOff = 5 * staffSpace;
+    _yOff = 3 * staffSpace;
     _black.style = PaintingStyle.stroke;
-    _black.strokeWidth = EngravingDefaults.staffLineThickness * staffSpace;
+    _black.strokeWidth = staffLineThickness * staffSpace;
     for (int line = 0; line < 5; line++) {
-      canvas.drawLine(Offset(0, yOff + line * staffSpace), Offset(size.width, yOff + line * staffSpace), _black);
+      canvas.drawLine(Offset(0, _yOff + line * staffSpace), Offset(size.width, _yOff + line * staffSpace), _black);
     }
 
-    double xOff = bassClef(canvas, yOff);
-    _grey.strokeWidth = EngravingDefaults.stemThickness * staffSpace;
-    canvas.drawLine(Offset(xOff, yOff), Offset(xOff, yOff + 4 * staffSpace), _grey);
+    _xOff = 10;
+    _xOff = bassClef(canvas, _yOff);
+
+    double staffPosition = 3.5;
+
+
+    canvas.drawRect(
+        Rect.fromLTRB(
+            _xOff + (GlyphBBoxesNoteheadBlack.bBoxNE.x- EngravingDefaults.stemThickness/2)*staffSpace,
+            _yOff +
+                (staffPosition  - GlyphBBoxesStem.bBoxNE.y- EngravingDefaults.stemThickness ) *
+                    staffSpace,
+            _xOff + (GlyphBBoxesNoteheadBlack.bBoxNE.x- EngravingDefaults.stemThickness/2 + 2)*staffSpace,
+            _yOff +
+                (staffPosition - GlyphBBoxesStem.bBoxNE.y- EngravingDefaults.stemThickness +EngravingDefaults.beamThickness) *
+                    staffSpace),
+        _blackFill);
+
+    noteHead(noteheadBlack, GlyphBBoxesNoteheadBlack.bBoxSW.x, staffPosition);
+    noteHead(stem, GlyphBBoxesNoteheadBlack.bBoxNE.x- EngravingDefaults.stemThickness/2,
+        staffPosition- EngravingDefaults.stemThickness);
+    _xOff += 2 * staffSpace;
+
+    noteHead(noteheadBlack, GlyphBBoxesNoteheadBlack.bBoxSW.x, staffPosition);
+    noteHead(stem, GlyphBBoxesNoteheadBlack.bBoxNE.x- EngravingDefaults.stemThickness/2,
+        staffPosition- EngravingDefaults.stemThickness);
+    _xOff += 2* staffSpace;
+
+    staffPosition = 0.0;
+
+    noteHead(noteheadBlack, GlyphBBoxesNoteheadBlack.bBoxSW.x, staffPosition);
+    noteHead(stem, GlyphBBoxesNoteheadBlack.bBoxSW.x + EngravingDefaults.stemThickness / 2,
+        staffPosition  +GlyphBBoxesStem.bBoxNE.y + EngravingDefaults.stemThickness);
+    _xOff += (GlyphBBoxesNoteheadBlack.bBoxNE.x - GlyphBBoxesNoteheadBlack.bBoxSW.x + 0.25) * staffSpace;
+
+    noteHead(noteWhole, GlyphBBoxesNoteheadBlack.bBoxSW.x, staffPosition+0.5);
+    _xOff += 2*staffSpace;
+        noteHead(noteHalfUp, GlyphBBoxesNoteheadBlack.bBoxSW.x, staffPosition+1);
+    _xOff += 2*staffSpace;
+    noteHead(noteHalfDown, GlyphBBoxesNoteheadBlack.bBoxSW.x, staffPosition+1.5);
+    _xOff += 2*staffSpace;
+    noteHead(noteQuarterUp, GlyphBBoxesNoteheadBlack.bBoxSW.x, staffPosition+2);
+    _xOff += 2*staffSpace;
+    noteHead(noteQuarterDown, GlyphBBoxesNoteheadBlack.bBoxSW.x, staffPosition);
+    _xOff += 2*staffSpace;
+    noteHead(note8thUp, GlyphBBoxesNoteheadBlack.bBoxSW.x, staffPosition+3);
+    _xOff += 3*staffSpace;
+    noteHead(note8thDown, GlyphBBoxesNoteheadBlack.bBoxSW.x, staffPosition);
+    _xOff += 2*staffSpace;
+    noteHead(note16thUp, GlyphBBoxesNoteheadBlack.bBoxSW.x, staffPosition+2.5);
+    _xOff += 3*staffSpace;
+    noteHead(note16thDown, GlyphBBoxesNoteheadBlack.bBoxSW.x, staffPosition);
+    _xOff += 3*staffSpace;
+    noteHead(barlineSingle, GlyphBBoxesNoteheadBlack.bBoxSW.x, 4);
+    _xOff += 2*staffSpace;
+    noteHead(restWhole, GlyphBBoxesNoteheadBlack.bBoxSW.x, 1);
+    _xOff += 3*staffSpace;
+    noteHead(restHalf, GlyphBBoxesNoteheadBlack.bBoxSW.x, 2);
+    _xOff += 3*staffSpace;
+    noteHead(restQuarter, GlyphBBoxesNoteheadBlack.bBoxSW.x, 2);
+    _xOff += 3*staffSpace;
+    noteHead(rest8th, GlyphBBoxesNoteheadBlack.bBoxSW.x, 2);
+    _xOff += 3*staffSpace;
+    noteHead(rest16th, GlyphBBoxesNoteheadBlack.bBoxSW.x, 2);
+    _xOff += 3*staffSpace;
+
   }
 
   @override
@@ -157,6 +223,13 @@ class _PlotPainter extends CustomPainter {
 
   double bassClef(Canvas canvas, final double yOff) {
     const double xOff = 0.5 * staffSpace;
+    final double fontSize = (GlyphBBoxesFClef.bBoxNE.y - GlyphBBoxesFClef.bBoxSW.y) * staffSpace;
+
+    //    where should it be
+//    canvas.drawRect(
+//        Rect.fromLTWH(xOff, yOff, (GlyphBBoxesFClef.bBoxNE.x - GlyphBBoxesFClef.bBoxSW.x) * staffSpace,
+//            (GlyphBBoxesFClef.bBoxNE.y - GlyphBBoxesFClef.bBoxSW.y) * staffSpace),
+//        _grey);
 
     TextPainter(
       text: TextSpan(
@@ -164,16 +237,16 @@ class _PlotPainter extends CustomPainter {
         style: TextStyle(
           fontFamily: 'Bravura',
           color: _black.color,
-          fontSize: (GlyphBBoxesFClef.bBoxNE.y - GlyphBBoxesFClef.bBoxSW.y) * staffSpace,
+          fontSize: fontSize,
         ),
       ),
       textDirection: TextDirection.ltr,
     )
       ..layout(
         minWidth: 0,
-        maxWidth: (GlyphBBoxesFClef.bBoxNE.x - GlyphBBoxesFClef.bBoxSW.x) * staffSpace,
+        maxWidth: fontSize,
       )
-      ..paint(canvas, Offset(xOff, -1.25 * staffSpace));
+      ..paint(canvas, Offset(xOff, yOff - 2 * fontSize + GlyphBBoxesFClef.bBoxNE.y * staffSpace));
 
     return xOff + GlyphBBoxesFClef.bBoxNE.x * staffSpace;
   }
@@ -201,12 +274,55 @@ class _PlotPainter extends CustomPainter {
     return xOff + GlyphBBoxesGClef.bBoxNE.x * staffSpace;
   }
 
-//  class  GlyphBBoxesGClef {
-//  static final Point<double> bBoxNE = Point( 2.684, 4.392 );
-//  static final Point<double> bBoxSW = Point( 0.0, -2.632 );
+  void noteHead(final String text, double x, double staffPosition) {
+    double w = 4*staffSpace;
+    TextPainter(
+      text: TextSpan(
+        text: text,
+        style: TextStyle(
+          fontFamily: 'Bravura',
+          color: _black.color,
+          fontSize: w,
+        ),
+      ),
+      textDirection: TextDirection.ltr,
+    )
+      ..layout(
+        minWidth: 0,
+        maxWidth: w,
+      )
+      ..paint(_canvas, Offset(_xOff + x * staffSpace, (_yOff ?? 0) + -2*w + (staffPosition - 0.05) * staffSpace));
+  }
 
+  Canvas _canvas;
+  double _xOff;
+  double _yOff;
 }
+
+final String noteheadWhole = '\uE0A2';
+final String noteheadBlack = '\uE0A4';
+final String stem = '\uE210';
+final String brace = '\uE000';
+final String barlineSingle = '\uE030';
+final String timeSigCommon = '\uE08A';
+final String noteWhole = '\uE1D2';
+final String noteHalfUp = '\uE1D3';
+final String noteHalfDown = '\uE1D4';
+final String noteQuarterUp = '\uE1D5';
+final String noteQuarterDown = '\uE1D6';
+final String note8thUp = '\uE1D7';
+final String note8thDown = '\uE1D8';
+final String note16thUp = '\uE1D9';
+final String note16thDown = '\uE1DA';
+final String restWhole = '\uE4E3';
+final String restHalf = '\uE4E4';
+final String restQuarter = '\uE4E5';
+final String rest8th = '\uE4E6';
+final String rest16th = '\uE4E7';
+
 
 final _white = Paint()..color = Colors.white;
 final _grey = Paint()..color = Colors.grey;
 final _black = Paint()..color = Colors.black;
+final _blackFill = Paint()..color = Colors.black .. style= PaintingStyle.fill;
+
